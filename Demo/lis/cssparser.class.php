ed22c778f41f2660cb9ed036b422c38c1130147f
jpasqualini75@gmail.com <jpasqualini75@gmail.com@fdf1a367-db3f-eb0e-3d02-b01e361201ca>
2011-11-05 00:13:59 +0000

    Documentation du code

--- Demo/lis/cssparser.class.php
+++ Demo/lis/cssparser.class.php
@@ -1,9 +1,31 @@
 <?php
+/*
+ Classe pour parser les fichier de définition de design css
+ @author internet
+ @version InDev
+ @license GPL
+*/
 class CssParseur {
+  
+  /*
+   @access public
+   @var Array Contient les définitions css
+  */
   public $css;
+  
+  /*
+   @access private
+   @staticvar CssParseur Contient l'instance actuelle du parseur
+  */
   private static $instance;
   
-  function ParseStr($str) {
+  /*
+   Parse une chaine contenant du css et retourne un tableau de propriété
+   @access public
+   @param string $str chaine css
+   @return Array Retourne les propriétés css
+  */
+  public function ParseStr($str) {
     $selecteur=array();
     // Remove comments
     $str = preg_replace("/\/\*(.*)?\*\//Usi", "", $str);
@@ -64,13 +86,27 @@ class CssParseur {
     return $selecteur;
   }
   
+  /*
+   Recupére l'instance unique du parseur css
+   @return CssParseur Retourne l'instance unique du parseur
+  */
   public static function GetInstance()
   {
+	// Si le parseur est déjà inititialisé alors on retourne son instance
   	if(CssParseur::$instance != null) return CssParseur::$instance;
+	
+	// Sinon on initialise le parseur et on retourne son instance
   	return new CssParseur();
   }
   
-  function Parse($filename) {
+  /*
+    Méthode pour parser un css qui retourne un tableau de propriété
+    @static
+    @access public
+    @param string $filename
+    @return Array Retourne le tableau de propriété
+  */
+  public function Parse($filename) {
       return $this->ParseStr(file_get_contents($filename));
   }
   
